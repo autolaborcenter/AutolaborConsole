1d10817397dbefa479b178cbe5d4c3acca72aaea
YangDR <ydrml@hotmail.com>
2018-12-06 16:59:51 +0800

    Initial commit

---
+++ Autolabor Console/MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Autolabor_Console {
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow {
+        private byte _sequence; // 发送指令序列号
+        private long _taskSeq; // 速度控制协程序列号
+
+        // 处理指令的状态机
+        private readonly SerialReader _reader = new SerialReader();
+
+        // 串口
+        private readonly SerialPort _serial
+            = new SerialPort {
+                PortName = "COM1",
+                BaudRate = 115200
+            };
+
+        public MainWindow() {
+            InitializeComponent();
+
+            _serial.DataReceived +=
+                (sender, e) => {
+                    var (ok, command) = _reader.Read((SerialPort) sender);
+                    if (ok) {
+                        var payload = command.Value.Payload;
+                        switch (command.Value.Id) {
+                            case 1: // 解析编码器
+                                var left = payload[0] << 8 | payload[1];
+                                var right = payload[2] << 8 | payload[3];
+                                Dispatcher.Invoke(() => EncoderBlock.Text = $"左轮：{left}  |  右轮：{right}");
+                                break;
+                            case 2: // 解析电量
+                                Dispatcher.Invoke(() => BatteryBlock.Text = payload[0].ToString());
+                                break;
+                            case 0xFF: // 超时，重置状态
+                                Send(new byte[] {5, 0});
+                                break;
+                        }
+
+                        Dispatcher.Invoke(() => UpdateRespond(command.Value.ToString()));
+                    }
+                    else if (command.HasValue)
+                        Dispatcher.Invoke(() => UpdateRespond("received but check failed"));
+                };
+
+            Scan();
+        }
+
+        /// <summary>
+        /// 	更新返回列表
+        /// </summary>
+        /// <param name="text">内容</param>
+        private void UpdateRespond(string text)
+            => Dispatcher.Invoke
+            (() => {
+                lock (RespondView) {
+                    if (RespondView.Items.Count > 20)
+                        RespondView.Items.RemoveAt(0);
+                    RespondView.Items.Add(text);
+                }
+            });
+
+        /// <summary>
+        /// 	扫描串口
+        /// </summary>
+        private void Scan()
+            => Dispatcher.Invoke
+            (() => {
+                lock (SerialsBox) {
+                    SerialsBox.Items.Clear();
+                    foreach (var port in SerialPort.GetPortNames())
+                        SerialsBox.Items.Add(port);
+
+                    if (SerialsBox.Items.Count > 0)
+                        SerialsBox.SelectedIndex = 0;
+                }
+            });
+
+        /// <summary>
+        /// 	发送一条指令
+        /// </summary>
+        /// <param name="payload">指令内容</param>
+        private void Send(byte[] payload) {
+            Debug.Assert(payload.Length < 256);
+            lock (_serial) {
+                if (!_serial.IsOpen) return;
+                var stream = new MemoryStream(5 + payload.Length);
+                stream.WriteByte(0x55);
+                stream.WriteByte(0xAA);
+                stream.WriteByte((byte) payload.Length);
+                stream.WriteByte(_sequence++);
+                stream.Write(payload, 0, payload.Length);
+
+                stream.WriteByte(stream
+                    .GetBuffer()
+                    .Take(stream.Capacity - 1)
+                    .Aggregate((tail, it) => (byte) (tail ^ it)));
+
+                _serial.Write(stream.GetBuffer(), 0, stream.Capacity);
+                Thread.Sleep(20);
+            }
+        }
+
+        private void Scan_Click(object sender, RoutedEventArgs e) => Scan();
+
+        /// <summary>
+        /// 	开始一个协程，每秒询问一次电量
+        /// </summary>
+        private async Task RequireBattery() {
+            while (_serial.IsOpen) {
+                await Task.Delay(1000);
+                Send(new byte[] {2, 0});
+            }
+        }
+
+        /// <summary>
+        /// 	开始一个协程，每100毫秒询问一次编码器
+        /// </summary>
+        private (int, int) Velocity {
+            set {
+                var seq = Interlocked.Increment(ref _taskSeq);
+                var v = 8 * (VelocityBox.SelectedIndex + 1);
+
+                var (left, right) = value;
+                left *= v;
+                right *= v;
+
+                Task.Run(async () => {
+                    while (_serial.IsOpen && seq == _taskSeq) {
+                        await Task.Delay(100);
+                        Send(new byte[] {
+                            1,
+                            (byte) (left >> 8), (byte) left,
+                            (byte) (right >> 8), (byte) right,
+                            0, 0, 0, 0
+                        });
+                    }
+                });
+            }
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e) {
+            lock (_serial) {
+                if (_serial.IsOpen) {
+                    _serial.Close();
+                    ((Button) sender).Content = "打开";
+                }
+                else if (SerialsBox.SelectedItem != null) {
+                    _serial.PortName = (string) SerialsBox.SelectedItem;
+                    try {
+                        _serial.Open();
+
+                        Task.Run(RequireBattery);
+                        Velocity = (0, 0);
+
+                        ((Button) sender).Content = "关闭";
+                    }
+                    catch (UnauthorizedAccessException) { }
+                    catch (IOException) { }
+                }
+            }
+        }
+
+        private void Clear_OnClick(object sender, RoutedEventArgs e) {
+            lock (RespondView) {
+                RespondView.Items.Clear();
+            }
+        }
+
+        private void Drive_OnClick(object sender, RoutedEventArgs e) {
+            if (!_serial.IsOpen) return;
+            var value = ((sender as Control)?.Tag as string)?.Split(' ') ?? throw new Exception();
+            Velocity = (Convert.ToInt16(value[0]), Convert.ToInt16(value[1]));
+        }
+
+        private void ClearEncoder_OnClick(object sender, RoutedEventArgs e) {
+            Send(new byte[] {6, 0});
+            Dispatcher.Invoke(() => EncoderBlock.Text = "(0, 0)");
+        }
+    }
+}
---
+++ Autolabor Console/SerialReader.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+
+namespace Autolabor_Console {
+	public static class Functions {
+		public static U Let<T, U>(this T receiver, Func<T, U> block) => block(receiver);
+
+		public static T Also<T>(this T receiver, Action<T> block) {
+			block(receiver);
+			return receiver;
+		}
+
+		/// <summary>
+		///     按范围拷贝数组
+		/// </summary>
+		/// <param name="receiver">原数组</param>
+		/// <param name="begin">起始坐标（包括）</param>
+		/// <param name="end">结束坐标（不包括）</param>
+		/// <typeparam name="T">数组类型，引用类型浅拷贝</typeparam>
+		/// <returns>新数组</returns>
+		public static T[] CopyRange<T>(
+			this T[] receiver,
+			int      begin = 0,
+			int      end   = int.MaxValue
+		) => new T[Math.Min(end, receiver.Length) - begin]
+		   .Also(it => Array.Copy(receiver, begin, it, 0, it.Length));
+	}
+
+
+	public struct Command {
+		public readonly byte   Id;
+		public readonly byte   Seq;
+		public readonly byte[] Payload;
+
+		public Command(byte id, byte seq, byte[] payload) {
+			Id      = id;
+			Seq     = seq;
+			Payload = payload;
+		}
+
+		public override string ToString() {
+			var builder = new StringBuilder();
+			builder.Append($"Id: {Id}, Seq: {Seq}, Payload: ");
+			foreach (var b in Payload) {
+				builder.Append(Convert.ToString(b, 16));
+				builder.Append(' ');
+			}
+
+			return builder.ToString();
+		}
+	}
+
+
+	public class SerialReader {
+		private          short  _state;
+		private          byte[] _buffer;
+		private readonly Object _lock = new object();
+
+
+		public (bool, Command?) Read(SerialPort port) {
+			lock (_lock) {
+				while (true) {
+					var @int = port.ReadByte();
+					if (@int == -1) return (false, null);
+
+					var @byte = (byte) @int;
+					switch (_state) {
+						case 0:
+							if (@byte == 0x55) ++_state;
+							break;
+						case 1:
+							if (@byte == 0xAA) ++_state;
+							break;
+						case 2:
+							if (@byte != 2 && @byte != 9) {
+								_state = 0;
+								break;
+							}
+
+							_buffer    = new byte[@byte + 5];
+							_buffer[0] = 0x55;
+							_buffer[1] = 0xAA;
+							_buffer[2] = @byte;
+							++_state;
+							break;
+						default:
+							_buffer[_state++] = @byte;
+							if (_state != _buffer.Length) break;
+							_state = 0;
+							return _buffer
+							      .Take(_buffer.Length - 1)
+							      .Aggregate((tail, it) => (byte) (tail ^ it))
+							      .Let(it => it == @byte)
+								       ? (true, new Command(_buffer[4],
+								                            _buffer[3],
+								                            _buffer.CopyRange(5, _buffer.Length - 1)))
+								       : (false, new Command(0, 0, _buffer));
+					}
+				}
+			}
+		}
+	}
+}
